d8fd41542ed72fff9ed5fa20a4809521df378f84
Pratosh <pratoshbhadoriya@Pratoshs-MacBook-Air.local>
2024-05-01 00:43:57 +0530

    chapter 2

--- App.js
+++ App.js
@@ -1,3 +1,16 @@
-const heading = React.createElement("h1", {}, "hello pratosh");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = <h1>Hi pratosh</h1>; //react element
+const Title = () => <h1>Title </h1>;
+const HeadingComponent = () => {
+  return (
+    <div>
+      <Title />
+      <h2>Hi pratosh bhai   </h2>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent />);
